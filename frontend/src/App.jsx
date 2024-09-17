42f766bec9a51671f3a0d977c4f2c53101bfbc8a
rohitjaiswal-3878 <rj710910@gmail.com>
2024-09-18 00:48:51 +0530

    Frontend: Created homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return <h1>Hellow world</h1>;
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Homepage/index.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styles from "./index.module.css";
+import allIcon from "../../assets/all.png";
+import medicalIcon from "../../assets/medical.png";
+import fruitsIcon from "../../assets/fruits.png";
+import worldIcon from "../../assets/world.png";
+import indiaIcon from "../../assets/india.png";
+
+function Homepage() {
+  const categories = [
+    {
+      title: "All",
+      image: allIcon,
+    },
+    {
+      title: "Medical",
+      image: medicalIcon,
+    },
+    {
+      title: "Fruits",
+      image: fruitsIcon,
+    },
+    {
+      title: "World",
+      image: worldIcon,
+    },
+    {
+      title: "India",
+      image: indiaIcon,
+    },
+  ];
+  return (
+    <div className={styles.homepage}>
+      <div className={styles.navbar}>
+        <div className={styles.buttons}>
+          <div>Register Now</div>
+          <div className={styles.green}>Sign In</div>
+        </div>
+      </div>
+
+      <div className={styles.stories}>
+        <ul className={styles.categories}>
+          {categories.map((category, index) => (
+            <li key={index}>
+              <img src={category.image} alt="" />
+              <span>{category.title}</span>
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles.topStories}>
+          <h3>Top Stories About food</h3>
+          <div className={styles.allStories}>
+            <span>No stories Available</span>
+          </div>
+        </div>
+
+        <div className={styles.topStories}>
+          <h3>Top Stories About food</h3>
+          <div className={styles.allStories}>
+            <span>No stories Available</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
